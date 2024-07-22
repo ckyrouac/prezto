41a8b3295254149b703feacafd5cf5d8c6788a14
Chris Kyrouac <ckyrouac@redhat.com>
2024-07-22 13:15:48 -0400

    Store prezto config in /etc

--- init.zsh
+++ init.zsh
@@ -165,11 +165,11 @@ function pmodload {
 # to rely on dirty hacks to force prezto into a directory. Additionally, it
 # needs to be done here because inside the pmodload function ${0:h} evaluates to
 # the current directory of the shell rather than the prezto dir.
-ZPREZTODIR=${0:h}
+ZPREZTODIR="/etc/zprezto"
 
 # Source the Prezto configuration file.
-if [[ -s "${ZDOTDIR:-$HOME}/.zpreztorc" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zpreztorc"
+if [[ -s "/etc/zpreztorc" ]]; then
+  source "/etc/zpreztorc"
 fi
 
 # Disable color and theme in dumb terminals.
